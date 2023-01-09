4a3bb162fe24a10c844d5f539a6721b840591f9d
Mohamed_Anwar <alhamed202024@gmail.com>
2023-01-09 17:14:50 +0300

    Anonymous Function and Use Cases

--- Function.js
+++ Function.js
@@ -15,3 +15,31 @@ function calc(...numbers) {
   }
   
   console.log(calc(10, 20, 10, 30, 50, 30));
+
+  /*
+  Function
+  - Anonymous Function
+  - Calling Named Function vs Anonymous Function
+  - Argument To Other Function
+  - Task Without Name
+  - SetTimeout
+*/
+
+let calculator = function (num1, num2) {
+    return num1 + num2;
+  };
+  
+  console.log(calculator(10, 20));
+  
+  function sayHello() {
+    console.log("Hello Osama");
+  }
+  
+  document.getElementById("show").onclick = sayHello;
+  document.getElementById("show").onclick = function () {
+    console.log("Hello Osama");
+  }
+  
+  setTimeout(function elzero() {
+    console.log("Good");
+  }, 2000);
