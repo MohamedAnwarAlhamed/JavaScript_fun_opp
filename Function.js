5833f28bc52ec1222feb8f43e148f363aba6c38f
Mohamed_Anwar <alhamed202024@gmail.com>
2023-01-09 17:25:04 +0300

    Return Nested Function

--- Function.js
+++ Function.js
@@ -43,3 +43,52 @@ let calculator = function (num1, num2) {
   setTimeout(function elzero() {
     console.log("Good");
   }, 2000);
+
+  /*
+  Function
+  - Function Inside Function
+  - Return Function
+*/
+
+// Example 1
+
+function sayMessage(fName, lName) {
+  let message = `Hello`;
+  // Nested Function
+  function concatMsg() {
+    message = `${message} ${fName} ${lName}`;
+  }
+  concatMsg();
+  return message;
+}
+
+console.log(sayMessage("Osama", "Mohamed"));
+
+// Example 2
+
+function sayMessage(fName, lName) {
+  let message = `Hello`;
+  // Nested Function
+  function concatMsg() {
+    return `${message} ${fName} ${lName}`;
+  }
+  return concatMsg();
+}
+
+console.log(sayMessage("Osama", "Mohamed"));
+
+// Example 3
+
+function sayMessage(fName, lName) {
+  let message = `Hello`;
+  // Nested Function
+  function concatMsg() {
+    function getFullName() {
+      return `${fName} ${lName}`;
+    }
+    return `${message} ${getFullName()}`;
+  }
+  return concatMsg();
+}
+
+console.log(sayMessage("Osama", "Mohamed"));
