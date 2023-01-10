c9e03b12b33c9a08de4ee78480505663eaa52d84
Mohamed_Anwar <alhamed202024@gmail.com>
2023-01-10 17:49:53 +0300

    Using Length With Array

--- Array.js
+++ Array.js
@@ -22,3 +22,16 @@ myFriends[3] = ["Sameh", "Ameer"];
 console.log(myFriends);
 
 console.log(Array.isArray(myFriends));
+
+/*
+  Array Methods
+  - Length
+*/
+
+// Index Start From 0 [ 0, 1, 2, 3, 4 ]
+
+let myFriends = ["Ahmed", "Mohamed", "Sayed", "Alaa"];
+
+myFriends.length = 2;
+
+console.log(myFriends);
