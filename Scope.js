0d1cf424a04b1d0d0169f65c31d8a94381221009
Mohamed_Anwar <alhamed202024@gmail.com>
2023-01-10 16:56:08 +0300

    Scope – Block

--- Scope.js
+++ Scope.js
@@ -17,3 +17,17 @@ console.log(`From Global ${a}`);
 console.log(`From Global ${b}`);
 
 showText();
+
+/*
+  Scope
+  - Block Scope [If, Switch, For]
+*/
+
+var x = 10;
+
+if (10 === 10) {
+  let x = 50;
+  console.log(`From If Block ${x}`);
+}
+
+console.log(`From Global ${x}`);
