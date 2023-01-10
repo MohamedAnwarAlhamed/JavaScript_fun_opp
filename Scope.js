e9e03b9503699cd7fda63af780e503476a24fc8b
Mohamed_Anwar <alhamed202024@gmail.com>
2023-01-10 17:03:02 +0300

    Scope – Lexical (Static)

--- Scope.js
+++ Scope.js
@@ -31,3 +31,30 @@ if (10 === 10) {
 }
 
 console.log(`From Global ${x}`);
+
+/*
+  Scope
+  - Lexical Scope
+
+  Search
+  - Execution Context
+  - Lexical Environment
+*/
+
+function parent() {
+    let a = 10;
+  
+    function child() {
+      console.log(a);
+      console.log(`From Child ${b}`);
+  
+      function grand() {
+        let b = 100;
+        console.log(`From Grand ${a}`);
+        console.log(`From Grand ${b}`);
+      }
+      grand();
+    }
+    child();
+  }
+  parent();
