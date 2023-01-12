79e41feb368c5171180b85ebda14c712478fa91f
Mohamed_Anwar <alhamed202024@gmail.com>
2023-01-12 17:23:25 +0300

    Dot Notation vs Bracket Notation

--- Object.js
+++ Object.js
@@ -18,3 +18,22 @@ let user = {
   console.log(user.theName);
   console.log(user.theAge);
   console.log(user.sayHello());
+
+  /*
+  Object
+  - Dig Deeper
+  - Dot Notation vs Bracket Notation
+  - Dynamic Property Name
+*/
+
+let myVar = "country";
+
+let user = {
+  theName: "Osama",
+  country: "Egypt",
+};
+
+console.log(user.theName);
+console.log(user.country); // user.country
+console.log(user.myVar); // user.country
+console.log(user[myVar]); // user.country
