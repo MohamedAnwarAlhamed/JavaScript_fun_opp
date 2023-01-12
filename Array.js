0fce000f918d9f48eeef442af5fdde2584298ccf
Mohamed_Anwar <alhamed202024@gmail.com>
2023-01-12 16:35:50 +0300

    Sorting Array

--- Array.js
+++ Array.js
@@ -95,3 +95,13 @@ if (myFriends.lastIndexOf("Osama") === -1) {
 console.log(myFriends.indexOf("Osama"));
 console.log(myFriends.lastIndexOf("Osama"));
 
+/*
+  Arrays Methods [Sort]
+  - sort(Function [Opt])
+  - reverse
+*/
+
+let myFriends = [10, "Sayed", "Mohamed", "90", 9000, 100, 20, "10", -20, -10];
+
+console.log(myFriends);
+console.log(myFriends.sort().reverse());
