7e600c2648d20fcecfdf1b78285f4ade29fa8019
Mohamed_Anwar <alhamed202024@gmail.com>
2023-01-12 16:46:10 +0300

    Joining Arrays

--- Array.js
+++ Array.js
@@ -131,3 +131,24 @@ console.log(myFriends);
 myFriends.splice(1, 2, "Sameer", "Samara");
 
 console.log(myFriends);
+
+/*
+  Arrays Methods [Joining]
+  - concat(array, array) => Return A New Array
+  - join(Separator)
+*/
+
+let myFriends = ["Ahmed", "Sayed", "Ali", "Osama", "Gamal", "Ameer"];
+let myNewFriends = ["Samar", "Sameh"];
+let schoolFriends = ["Haytham", "Shady"];
+
+let allFriends = myFriends.concat(myNewFriends, schoolFriends, "Gameel", [1, 2]);
+
+console.log(allFriends);
+
+console.log(allFriends.join());
+console.log(allFriends.join(""));
+console.log(allFriends.join(" @ "));
+console.log(allFriends.join("|"));
+console.log(allFriends.join("|").toUpperCase());
+
