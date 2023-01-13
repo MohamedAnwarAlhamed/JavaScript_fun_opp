d06ad4801d2827cf59ac6646dbdbcbaaeeff4594
Mohamed_Anwar <alhamed202024@gmail.com>
2023-01-13 22:24:51 +0300

    Create Object With New Keyword

--- Object.js
+++ Object.js
@@ -79,3 +79,28 @@ console.log(user["addresses"]["egypt"]);
 console.log(user["addresses"]["egypt"]["one"]);
 
 console.log(user.checkAv());
+
+
+
+/*
+  Object
+  - Create With New Keyword new Object();
+*/
+
+let user = new Object({
+  age: 20,
+});
+
+console.log(user);
+
+user.age = 38;
+user["country"] = "Egypt";
+
+user.sayHello = function () {
+  return `Hello`;
+};
+
+console.log(user);
+console.log(user.age);
+console.log(user.country);
+console.log(user.sayHello());
