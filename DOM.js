d9bbc2c5a4f4ae7091aac29e05c0d7322f183e28
Mohamed_Anwar <alhamed202024@gmail.com>
2023-01-15 22:13:04 +0300

    Create And Append Elements

--- DOM.js
+++ DOM.js
@@ -97,3 +97,30 @@ if (document.getElementsByTagName("div")[0].hasAttributes()) {
 } else {
   console.log(`Div Has No Attributes`);
 }
+
+/*
+  DOM [Create Elements]
+  - createElement
+  - createComment
+  - createTextNode
+  - createAttribute
+  - appendChild
+*/
+
+let myElement = document.createElement("div");
+let myAttr = document.createAttribute("data-custom");
+let myText = document.createTextNode("Product One");
+let myComment = document.createComment("This Is Div");
+
+myElement.className = "product";
+myElement.setAttributeNode(myAttr);
+myElement.setAttribute("data-test", "Testing");
+
+// Append Comment To Element
+myElement.appendChild(myComment);
+
+// Append Text To Element
+myElement.appendChild(myText);
+
+// Append Element To Body
+document.body.appendChild(myElement);
