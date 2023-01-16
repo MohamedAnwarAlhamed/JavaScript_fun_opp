ebf2351e384d3a172caddafce3c02be9a6994420
Mohamed_Anwar <alhamed202024@gmail.com>
2023-01-16 17:10:10 +0300

    ClassList Object and Methods

--- DOM_Events.js
+++ DOM_Events.js
@@ -75,3 +75,26 @@ window.onload = function () {
 one.onblur = function () {
   document.links[0].click();
 };
+
+/*
+  DOM [Class List]
+  - classList
+  --- length
+  --- contains
+  --- item(index)
+  --- add
+  --- remove
+  --- toggle
+*/
+
+let element = document.getElementById("my-div");
+
+console.log(element.classList);
+console.log(typeof element.classList);
+console.log(element.classList.contains("osama"));
+console.log(element.classList.contains("show"));
+console.log(element.classList.item("3"));
+
+element.onclick = function () {
+  element.classList.toggle("show");
+};
