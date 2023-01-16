b5162887b4f249f369f6d1aa38d7515ebde280b9
Mohamed_Anwar <alhamed202024@gmail.com>
2023-01-16 17:40:10 +0300

    Before, After, Prepend, Append, Remove

--- DOM.js
+++ DOM.js
@@ -166,3 +166,17 @@ element.style.setProperty("font-size", "40px", "important");
 
 document.styleSheets[0].rules[0].style.removeProperty("line-height"); 
 document.styleSheets[0].rules[0].style.setProperty("background-color", "red", "important");
+
+/*
+  DOM [Deal With Elements]
+  - before [Element || String]
+  - after [Element || String]
+  - append [Element || String]
+  - prepend [Element || String]
+  - remove
+*/
+
+let element = document.getElementById("my-div");
+let createdP = document.createElement("p");
+
+// element.remove();
