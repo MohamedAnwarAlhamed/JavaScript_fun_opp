1cb89589d944778e49fb285ca194bb8df053e745
Mohamed_Anwar <alhamed202024@gmail.com>
2023-01-17 16:46:13 +0300

    DOM Cloning

--- DOM.js
+++ DOM.js
@@ -197,3 +197,16 @@ console.log(span.parentElement);
 span.onclick = function () {
   span.parentElement.remove();
 }
+
+
+/*
+  DOM [Cloning]
+  - cloneNode(Deep)
+*/
+
+let myP = document.querySelector("p").cloneNode(true);
+let myDiv = document.querySelector("div");
+
+myP.id = `${myP.id}-clone`;
+
+myDiv.appendChild(myP);
