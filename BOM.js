a397ff558f90ac6b08070f07577a394f5e9f6ea5
Mohamed_Anwar <alhamed202024@gmail.com>
2023-01-17 17:25:40 +0300

    setTimeout and clearTimeout

--- BOM.js
+++ BOM.js
@@ -42,3 +42,38 @@ window.document.title = "Hello JS";
 let promptMsg = prompt("Good Day To You?", "Write Day With 3 Characters");
 
 console.log(promptMsg);
+
+
+/*
+  BOM [Browser Object Model]
+  - setTimeout(Function, Timeout, Additional Params)
+  - clearTimeout(Identifier)
+*/
+
+// setTimeout(function () {
+//   console.log("Msg");
+// }, 3000);
+
+// setTimeout(sayMsg, 3000);
+
+// function sayMsg() {
+//   console.log(`Iam Message`);
+// }
+
+// setTimeout(sayMsg, 3000, "Osama", 38);
+
+// function sayMsg(user, age) {
+//   console.log(`Iam Message For ${user} Age Is : ${age}`);
+// }
+
+let btn = document.querySelector("button");
+
+btn.onclick = function () {
+  clearTimeout(counter);
+};
+
+function sayMsg(user, age) {
+  console.log(`Iam Message For ${user} Age Is : ${age}`);
+}
+
+let counter = setTimeout(sayMsg, 3000, "Osama", 38);
