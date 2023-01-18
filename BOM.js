48181e1033215c6ced0758bde2e86d110636ecc6
Mohamed_Anwar <alhamed202024@gmail.com>
2023-01-18 21:59:05 +0300

    Scroll, ScrollTo, ScrollBy, Focus, Print, Stop

--- BOM.js
+++ BOM.js
@@ -177,3 +177,21 @@ setTimeout(function () {
 */
 
 console.log(history);
+
+/*
+  BOM [Browser Object Model]
+  - stop()
+  - print()
+  - focus()
+  - scrollTo(x, y || Options)
+  - scroll(x, y || Options)
+  - scrollBy(x, y || Options)
+*/
+
+// let myNewWindow = window.open("https://google.com", "", "width=500,height=500");
+
+// window.scrollTo({
+//   left: 500,
+//   top: 200,
+//   behavior: "smooth"
+// });
