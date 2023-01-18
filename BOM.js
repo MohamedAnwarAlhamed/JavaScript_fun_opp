81975913526e35a1a2987284826bbcc8904df71c
Mohamed_Anwar <alhamed202024@gmail.com>
2023-01-18 21:38:28 +0300

    Window Open And Close

--- BOM.js
+++ BOM.js
@@ -136,3 +136,27 @@ console.log(location.href);
 // console.log(location.protocol);
 
 // console.log(location.hash);
+
+
+/*
+  BOM [Browser Object Model]
+  - open(URL [Opt], Window Name Or Target Attr [Opt], Win Features [Opt], History Replace [Opt])
+  - close()
+  - Window Features
+  --- left [Num]
+  --- top [Num]
+  --- width [Num]
+  --- height [Num]
+  --- menubar [yes || no]
+
+  Search
+  - Window.Open Window Features
+*/
+
+setTimeout(function () {
+  window.open("", "_self", "", false);
+}, 2000);
+
+setTimeout(function () {
+  window.open("https://google.com", "_blank", "width=400,height=400,left=200,top=10");
+}, 2000);
