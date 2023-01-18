8385cd0c0f91750ffe6ddf2c8629f4eb6d6fb19c
Mohamed_Anwar <alhamed202024@gmail.com>
2023-01-18 23:15:22 +0300

    Destructuring Arrays Part 2

--- Destructuring.js
+++ Destructuring.js
@@ -26,3 +26,19 @@ let [, y, , z] = myFriends;
 
 console.log(y);
 console.log(z);
+
+/*
+  Destructuring
+  - Destructuring Array Advanced Examples
+*/
+
+let myFriends = ["Ahmed", "Sayed", "Ali", ["Shady", "Amr", ["Mohamed", "Gamal"]]];
+
+// console.log(myFriends[3][2][1]);
+
+// let [, , , [a, , [, b]]] = myFriends;
+
+let [, , , [a, , [, b]]] = myFriends;
+
+console.log(a); // Shady
+console.log(b); // Gamal
