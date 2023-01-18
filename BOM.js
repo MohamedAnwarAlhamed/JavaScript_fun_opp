a67c95091481ec8a7d4ccd356baadd20ea160787
Mohamed_Anwar <alhamed202024@gmail.com>
2023-01-18 21:46:19 +0300

    Window History Object

--- BOM.js
+++ BOM.js
@@ -160,3 +160,20 @@ setTimeout(function () {
 setTimeout(function () {
   window.open("https://google.com", "_blank", "width=400,height=400,left=200,top=10");
 }, 2000);
+
+
+/*
+  BOM [Browser Object Model]
+  - History API
+  --- Properties
+  ------ length
+  --- Methods
+  ------ back()
+  ------ forward()
+  ------ go(Delta) => Position In History
+
+  Search [For Advanced Knowledge]
+  - pushState() + replaceState()
+*/
+
+console.log(history);
