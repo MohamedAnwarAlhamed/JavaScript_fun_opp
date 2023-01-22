83e62e9cd18cae03cf8303dc93d4f4bed5ad48df
Mohamed_Anwar <alhamed202024@gmail.com>
2023-01-22 17:24:34 +0300

    Update Properties & Built In Constructors

--- OOP.js
+++ OOP.js
@@ -99,3 +99,39 @@ class User {
   console.log(userTwo.s);
   console.log(userTwo.msg); // Native Code
   console.log(userTwo.writeMsg); // Native Code
+
+
+  /*
+  Constructor Function
+  - Update Properties
+  - Built In Constructors
+*/
+
+class User {
+  constructor(id, username, salary) {
+    this.i = id;
+    this.u = username;
+    this.s = salary;
+  }
+  updateName(newName) {
+    this.u = newName;
+  }
+}
+
+let userOne = new User(100, "Elzero", 5000);
+
+console.log(userOne.u);
+userOne.updateName("Osama");
+console.log(userOne.u);
+
+let strOne = "Elzero";
+let strTwo = new String("Elzero");
+
+console.log(typeof strOne);
+console.log(typeof strTwo);
+
+console.log(strOne instanceof String);
+console.log(strTwo instanceof String);
+
+console.log(strOne.constructor === String);
+console.log(strTwo.constructor === String);
