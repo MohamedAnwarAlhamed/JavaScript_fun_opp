541ef9802f0db5f2c95623437ad56ddc409b07a7
Mohamed_Anwar <alhamed202024@gmail.com>
2023-01-23 17:39:29 +0300

    Named vs Default Export And Import All

--- Modules.js
+++ Modules.js
@@ -11,3 +11,25 @@ function saySomething() {
 }
 
 export { a, arr, saySomething };
+
+
+/*
+  Modules
+  - Export Alias
+  - Named Export
+  - Default Export
+  - Import All
+*/
+
+let a = 10;
+let arr = [1, 2, 3, 4];
+
+function saySomething() {
+  return `Something`;
+}
+
+export { a as myNumber, arr, saySomething };
+
+export default function () {
+  return `Hello`;
+}
