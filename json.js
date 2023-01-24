be1b4bafccf8888eb9c699fd8a3233a4dc034009
Mohamed_Anwar <alhamed202024@gmail.com>
2023-01-24 16:54:57 +0300

    JSON Syntax And Compare With JS Object

--- json.js
+++ json.js
@@ -21,3 +21,22 @@
   = Not Support Comments   = Support Comments       =
   ===================================================
 */
+
+
+/*
+  JSON Syntax
+  - Data Added Inside Curly Braces {  }
+  - Data Added With Key : Value
+  - Key Should Be String Wrapped In Double Quotes
+  - Data Separated By Comma
+  - Square Brackets [] For Arrays
+  - Curly Braces {} For Objects
+
+  Available Data Types
+  - String
+  - Number
+  - Object
+  - Array
+  - Boolean Values
+  - null
+*/
