e0ec2090e750f72c1b58c0d36264ffcc41e7cc10
Mohamed_Anwar <alhamed202024@gmail.com>
2023-01-26 17:03:18 +0300

    Regular Expression Quantifiers Part 2

--- Regular_Expression.js
+++ Regular_Expression.js
@@ -196,3 +196,19 @@ console.log(nums.match(numsRe));
 let urls = "https://google.com http://www.website.net web.com"; // http + https
 let urlsRe = /(https?:\/\/)?(www.)?\w+.\w+/ig;
 console.log(urls.match(urlsRe));
+
+
+/*
+  Regular Expression
+
+  Quantifiers
+  n{x}   => Number of
+  n{x,y} => Range
+  n{x,}  => At Least x
+*/
+
+let serials = "S100S S3000S S50000S S950000S";
+
+console.log(serials.match(/s\d{3}s/ig)); // S[Three Number]S
+console.log(serials.match(/s\d{4,5}s/ig)); // S[Four Or Five Number]S
+console.log(serials.match(/s\d{4,}s/ig)); // S[At Least Four]S
