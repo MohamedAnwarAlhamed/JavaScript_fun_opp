5d9614b21ff6d8f01a5cbbdccb22c29bb63c636f
Mohamed_Anwar <alhamed202024@gmail.com>
2023-01-26 16:36:20 +0300

    Regular Expression – Modifiers

--- Regular_Expression.js
+++ Regular_Expression.js
@@ -21,3 +21,31 @@ let url = 'http://www.elzero.org/';
 let url = 'https://.elzero.org/';
 let url = 'https://www.elzero.org/';
 let url = 'https://www.elzero.org/?facebookid=asdasdasd';
+
+
+/*
+  Regular Expression
+
+  Syntax
+  /pattern/modifier(s);
+  new RegExp("pattern", "modifier(s)")
+
+  Modifiers => Flags
+  i => case-insensitive
+  g => global
+  m => Multilines
+
+  Search Methods
+  - match(Pattern)
+
+  Match
+  -- Matches A String Against a Regular Expression Pattern
+  -- Returns An Array With The Matches
+  -- Returns null If No Match Is Found.
+*/
+
+let myString = "Hello Elzero Web School I Love elzero";
+
+let regex = /elzero/ig;
+
+console.log(myString.match(regex));
