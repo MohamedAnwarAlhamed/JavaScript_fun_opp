f5cfb10572842635d689dcc17dbadbd613a31a19
Mohamed_Anwar <alhamed202024@gmail.com>
2023-01-26 16:52:41 +0300

    Regular Expression Character Classes Part 2

--- Regular_Expression.js
+++ Regular_Expression.js
@@ -153,3 +153,23 @@ let valid = /\w@\w.(com|net)/g;
 console.log(email.match(dot));
 console.log(email.match(word));
 console.log(email.match(valid));
+
+
+/*
+  Regular Expression
+  Character Classes
+  \b => matches at the beginning or end of a word.
+  \B => matches NOT at the beginning/end of a word.
+
+  Test Method
+  pattern.test(input)
+*/
+
+let names = "Sayed 1Spam 2Spam 3Spam Spam4 Spam5 Osama Ahmed Aspamo";
+let re = /(\bspam|spam\b)/ig;
+console.log(names.match(re));
+
+console.log(re.test(names));
+console.log(/(\bspam|spam\b)/ig.test("Osama"));
+console.log(/(\bspam|spam\b)/ig.test("1Spam"));
+console.log(/(\bspam|spam\b)/ig.test("Spam1"));
